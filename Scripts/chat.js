f4a8c99aed7f8b8574c56a3d8b718964e2f31314
cheredi <tatyanaamugo07@gmail.com>
2022-02-17 21:09:08 +0300

    m

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -10,3 +10,22 @@ function botMessage(){
   }
   
   botMessage();
+
+ var myElem=document.getElementById("click")
+myElem.addEventListener("click", function(){
+    let messagE="Show them how much you care with this offer"
+    document.getElementById("respo").innerHTML=`<p class="respo"><span>'${messagE}'</span></p>`
+    document.getElementById("respo").scrollIntoView(true);
+
+})
+
+var myElem=document.getElementById("click2")
+myElem.addEventListener("click", function(){
+    let messagE="Lets celebrate our connection this valentine"
+    document.getElementById("respo1").innerHTML=`<p class="respo1"><span>'${messagE}'</span></p>`
+    document.getElementById("respo1").scrollIntoView(true);
+})
+var my1=document.getElementById("respo")
+my1.addEventListener("click",function(){
+    onclick="location.href='https://www.lifehack.org/articles/lifestyle/how-to-enjoy-life-as-a-single-person-on-valentines-day.html'"
+})
