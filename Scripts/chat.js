6cc1174424d1be391a986a61ac75d7d53b7b7c2c
cheredi <tatyanaamugo07@gmail.com>
2022-02-17 10:17:55 +0300

    m

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -1,5 +1,9 @@
+//firstMessage
 function firstBotMessage(){
-    let firstMessage="Hi, I am chatbot!Its going to be valentine in a few days, do you have a valentine?"
+    let firstMessage="Hi, I am chatbot!"
     document.getElementById("botStarterMesssage").innerHTML = '<p class="botText"><span>' + firstMessage +'</span></p>'
 }
 firstBotMessage();
+
+
+
